5fb090ee59524089dbac91fa7cd42eb9b224b484
Borimino <markusandreassen1994@gmail.com>
2013-09-16 19:02:04 +0200

    Signed-off-by: Borimino <markusandreassen1994@gmail.com>

--- src/ma/FirstRobot.java
+++ src/ma/FirstRobot.java
@@ -1,8 +1,22 @@
 package ma;
 
-import robocode.Robot;
+import robocode.*;
 
 public class FirstRobot extends Robot
 {
-
+	public void run()
+	{
+		while(true)
+		{
+			ahead(100);
+			turnGunRight(360);
+			back(100);
+			turnGunRight(360);
+		}
+	}
+	
+	public void onScannedRobot(ScannedRobotEvent e)
+	{
+		fire(1);
+	}
 }
